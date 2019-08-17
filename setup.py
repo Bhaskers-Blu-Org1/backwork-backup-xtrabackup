ce19fbc020efeb2686dd1b8fc0d064e2f97d04f4
James Reeve <reevejamesd@gmail.com>
2019-08-17 19:30:32 -0400

    v0.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(HERE, 'README.md')) as f:
 
 setup(
     name="backwork-backup-xtrabackup",
-    version="0.1.1",
+    version="0.2.0",
     description="Backwork plug-in for Percona XtraBackup backups.",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
